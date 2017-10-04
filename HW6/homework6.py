cca718f70be3aad92428bb60f6b700cc75383985
Kamden Hafke <khafke@umich.edu>
2017-10-04 18:24:21 -0400

    starting repository

---
+++ HW6/homework6.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('http://py4e-data.dr-chuck.net/comments_35736.htm')
+html = urlopen(url, context=ctx).read()
+
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
